ac44284427711ec62f5f06f5a07c94d14d40c075
parrojys1 <parrojaysoncarl_bsit@plmun.edu.ph>
2025-02-15 22:42:59 +0800

    Submitting Python Week 04 - Laboratory # 12

---
+++ switch_ternary_operator.py
@@ -0,0 +1,17 @@
+day_messages = {
+    "monday": "Today is Monday.",
+    "tuesday": "Today is Tuesday.",
+    "wednesday": "Today is Wednesday.",
+    "thursday":"Today is Thursday.",
+    "friday":"Today is Friday.",
+    "saturday":"Today is Saturday.",
+    "sunday":"Today is Sunday.",
+}
+
+day = input("Enter a day of the week: ").strip().lower()
+message = day_messages.get(day, "invalid day entered.")
+
+day_type = "Weekend" if day in ("saturday", "sunday") else "Weekday"
+
+print(message)
+print(day_type + "!")
